3bb8ed2f9aaeca109bae08944b5a2aadea67028a
yurisi0212 <dev.yurisi@gmail.com>
2022-04-22 04:53:53 +0900

    authを実装

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
+
+                <div class="card-body">
+                    <p>宇宙サーバー内で/registerを実行し、会員登録してください。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
